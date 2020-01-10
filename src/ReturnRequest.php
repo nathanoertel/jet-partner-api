49bed6e7780b7ba990a773b039582eaa2503a064
Nathan Oertel <nathan@digitalclouddesigns.com>
2020-01-10 14:41:12 +0000

    Added ability to get all types of returns

--- src/ReturnRequest.php
+++ src/ReturnRequest.php
@@ -3,8 +3,14 @@ namespace JetAPI;
 
 class ReturnRequest extends AbstractRequest {
 
-	public function getCreated() {
-		return $this->get('/created');
+	public function getReturns($status) {
+		if(!in_array($status, array(
+			'created',
+			'inprogress',
+			'completed by merchant'
+		))) throw new InvalidArgumentException('status must be one of "created", "inprogress" or "completed by merchant", input was: '.$status);
+
+		return $this->get('/'.$status);
 	}
 	
 	public function getReturn($returnId) {
