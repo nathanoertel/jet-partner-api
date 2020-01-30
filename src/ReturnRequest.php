9df43cf1f87ba681c75ba4f6b307c89be6fa7ffb
Nathan Oertel <nathan@digitalclouddesigns.com>
2020-01-30 16:40:04 +0000

    Added setting of parameters

--- src/ReturnRequest.php
+++ src/ReturnRequest.php
@@ -3,14 +3,14 @@ namespace JetAPI;
 
 class ReturnRequest extends AbstractRequest {
 
-	public function getReturns($status) {
+	public function getReturns($status, $parameters = array()) {
 		if(!in_array($status, array(
 			'created',
 			'inprogress',
 			'completed by merchant'
 		))) throw new InvalidArgumentException('status must be one of "created", "inprogress" or "completed by merchant", input was: '.$status);
 
-		return $this->get('/'.$status);
+		return $this->get('/'.$status, $parameters);
 	}
 	
 	public function getReturn($returnId) {
